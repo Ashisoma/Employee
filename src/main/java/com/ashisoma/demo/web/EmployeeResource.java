ffa6a52cb2c5518637f9a0455d33ad5f4ac7edac
Ashisoma <okalionesmus@gmail.com>
2022-01-19 13:57:14 +0300

    controllers and exceptions

---
+++ src/main/java/com/ashisoma/demo/web/EmployeeResource.java
@@ -0,0 +1,52 @@
+package com.ashisoma.demo.web;
+
+import com.ashisoma.demo.models.Employee;
+import com.ashisoma.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+
+    @Autowired
+    private final EmployeeService employeeService;
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getEmployee(){
+        List<Employee> employeeList = employeeService.allEmployees();
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable("id") Long id){
+        Employee employeeList = employeeService.findEmployeeId(id);
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.saveEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.OK);
+    }
+
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<?> updateEmployee(@PathVariable("id") Long id){
+         employeeService.deleteEmployeeId(id);
+        return new ResponseEntity<>( HttpStatus.OK);
+    }
+}
